3983aa38f1033447a4953f7f0acffec62e459011
Wei Xian <howeixian2688@gmail.com>
2026-09-07 09:16:25 +0800

    Fix unsupported user avatar format and add FXML

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,48 +1,55 @@
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+/**
+ * Represents a dialog box consisting of an ImageView and a label.
+ */
 public class DialogBox extends HBox {
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        displayPicture = new ImageView(i);
-        displayPicture.setFitWidth(100);
-        displayPicture.setFitHeight(100);
-        displayPicture.setPreserveRatio(true);
-
-        this.setSpacing(10);
-        this.setAlignment(Pos.TOP_RIGHT);
-        this.setMaxWidth(Double.MAX_VALUE);
-
-        this.getChildren().addAll(text, displayPicture);
+        dialog.setText(text);
+        displayPicture.setImage(img);
     }
 
-    /**
-     * Flips the dialog box so the image is on the left and text is on the right.
-     */
+    /** Flips the dialog box so the image is on the left and text on the right. */
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
-        ObservableList<Node> items = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(items);
-        this.getChildren().setAll(items);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
-    public static DialogBox getUserDialog(String s, Image i) {
-        return new DialogBox(s, i);
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
     }
 
-    public static DialogBox getDukeDialog(String s, Image i) {
-        DialogBox dialogBox = new DialogBox(s, i);
-        dialogBox.flip();
-        return dialogBox;
+    public static DialogBox getDukeDialog(String text, Image img) {
+        DialogBox db = new DialogBox(text, img);
+        db.flip();
+        return db;
     }
 }
---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,50 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/** Controller for the main GUI. */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Duke duke;
+
+    // The original DaUser.png was a WebP file with the wrong extension.
+    // JavaFX 17 cannot decode WebP, so use the verified PNG supplied by the tutorial.
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.tutorial.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Duke instance. */
+    public void setDuke(Duke d) {
+        duke = d;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = duke.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+}
